c9ff40f5b565dea70b87c1c09a1fd01d463a559b
Dustin Chang <dustinlchang@gmail.com>
2026-02-22 00:14:33 -0800

    Finished 3.4 IntoIter

--- src/second.rs
+++ src/second.rs
@@ -16,8 +16,8 @@ struct Node<T> {
     next: Link<T>,
 }
 
-// impl<T> List<T> {
-impl<T: std::fmt::Debug> List<T> {
+// impl<T: std::fmt::Debug> List<T> {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
@@ -53,7 +53,7 @@ impl<T: std::fmt::Debug> List<T> {
     pub fn peek(&self) -> Option<&T> {
         self.head.as_ref().map(|node| {
             // 1. Print the value (requires T to implement Debug)
-            println!("Peeking at node value: {:?}", node.elem);
+            // println!("Peeking at node value: {:?}", node.elem);
 
             // 2. Return the reference as the final expression
             &node.elem
@@ -62,6 +62,10 @@ impl<T: std::fmt::Debug> List<T> {
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map((|node| &mut node.elem))
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 }
 
@@ -124,4 +128,30 @@ fn peek() {
     list.peek_mut().map(|value| *value = 42);
     assert_eq!(list.peek(), Some(&42));
     assert_eq!(list.pop(), Some(42));
+}
+
+// Tuple structs are an alternative form of struct,
+// useful for trivial wrappers around other types.
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
 }
